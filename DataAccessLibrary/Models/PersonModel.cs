a4794841829c600f03b870dab31509d9adfb14a9
Shubham <shutomar@deloitte.com>
2023-03-11 19:13:35 +0530

    init

---
+++ DataAccessLibrary/Models/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    public class PersonModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+    }
+}
